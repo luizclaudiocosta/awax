2002ef01a894b7861681d386abfe8bba98988692
Luiz Claudio Costa <1970luizcosta@gmail.com>
2019-12-26 18:41:47 -0300

    primeiro commit

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+//var $j = jQuery.noConflict();
+
+$(function(){
+
+
+/*
+	$('.seeurl').showRealLink();
+
+
+	$('#form').bind('submit', function(e){
+		e.preventDefault();
+
+		var txt = $(this).serialize();
+
+		$.ajax({
+			type:'POST',
+			url:'ajax.php',
+			data:txt,
+			dataType:'json',
+			success:function(j) {
+				$('.div').html('Dados do usuário: <b>'+ j.nome + ' / '+ j.senha + '</b> e o nome de usuário tem '+ j.qt_nome +' caracteres.');
+			}
+		});
+	})
+*/
+
+
+});
